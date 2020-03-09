0a67f3503413afc9ac5992cba6c5a68646080e81
Fipzz <43668537+Fipzz@users.noreply.github.com>
2020-03-09 15:17:43 +0100

    100% rigtigt

--- Client/src/main/ClientMain.java
+++ Client/src/main/ClientMain.java
@@ -13,10 +13,13 @@ import java.util.Scanner;
 public class ClientMain {
     public static void main(String[] args) throws Exception {
 
+
+
         String username, password;
-        boolean loggedIn = false,failed = false;
+        boolean loggedIn = false,failed = false, playagain = false;
         Scanner scan = new Scanner(System.in);
         //TODO login
+
 
         System.out.println("--- Velkommen til galgelegs spillet! Log ind for at fortsætte ---");
 
@@ -36,46 +39,135 @@ public class ClientMain {
             }
         }while(!loggedIn);
 
-
-        //TODO play
-
         JSONObject spillet;
 
-        System.out.println("Velkommen til Galgeleg!");
-
-        boolean erSpilletSlut = false;
-        String bogstavGættet;
+        do{
 
 
-        do {
-            spillet = Unirest.get("").asJson().getBody().getObject(); //  <--indsæt URL
-            System.out.println();
-            System.out.println("--- Top of the round ---");
-            System.out.println("Ordet du skal gætte er: "+ spillet.get("synligtord")); //indsæt synligt ord!
-            System.out.println("Du har gættet på: ");
+            System.out.println("Velkommen til Galgeleg!");
 
-            /*String gættedeBogstaver[] = spillet.getJSONArray("gættedeBogstaver"); // Print alle gættede bogstaver
+            boolean erSpilletSlut = false;
+            String bogstavGættet;
 
-            for (int i = 0; i < gættedeBogstaver.size, i++){
-                System.out.print(gættedeBogstaver[i]);
+
+            do {
+                spillet = Unirest.get("").asJson().getBody().getObject(); //  <--indsæt URL
+                System.out.println();
+                System.out.println("--- Top of the round ---");
+                System.out.println("Ordet du skal gætte er: "+ spillet.get("synligtord")); //indsæt synligt ord!
+                System.out.println("Du har gættet på: ");
+
+                String gættedeBogstaver[] = (String[]) spillet.get("usedLetters"); // Print alle gættede bogstaver
+
+                for (int i = 0; i < gættedeBogstaver.length; i++){
+                    System.out.print(gættedeBogstaver[i]);
+                }
+
+                System.out.println();
+
+                art(0); //TODO GET LIV
+
+                System.out.print("Gæt på et bogstav: ");
+
+                do {
+
+                    bogstavGættet = scan.next();
+
+                    bogstavGættet.trim().toLowerCase();
+
+                }while(bogstavGættet.matches("")||bogstavGættet.matches(" "));
+
+                Unirest.post("localhost:8080/game").body(bogstavGættet).asJson();
+
+                System.out.println("--- Round end ---");
+
+            }while(erSpilletSlut);
+
+            System.out.println("Spillet er slut. Vil du spille igen?");
+            System.out.println("yes/no?:");
+            if(scan.next().trim().toLowerCase().matches("yes")){
+
+                playagain = false;
+                //TODO play again
+
+            } else {
+
+                playagain = false;
+
             }
 
-            System.out.println();
+        }while (playagain);
 
-            //TODO ASCII ART
 
-            System.out.print("Gæt på et bogstav: ");
 
-            bogstavGættet = scan.next();
+    }
 
-            Unirest.post("").body("{"name":"Sam Baeldung", "city":"viena"}").asJson();
-            */
-            System.out.println("--- Round end ---");
+    private static void art(int liv){
 
-        }while(erSpilletSlut);
-
-        System.out.println("Spillet er slut. Vil du spille igen?");
-        System.out.println("yes/no?:");
+        if (liv == 6){
+            System.out.println("               ");
+            System.out.println("    -------    ");
+            System.out.println("    |     |    ");
+            System.out.println("          |    ");
+            System.out.println("          |    ");
+            System.out.println("          |    ");
+            System.out.println("          |    ");
+            System.out.println("        -----  ");
+        }else if(liv == 5){
+            System.out.println("               ");
+            System.out.println("    -------    ");
+            System.out.println("    |     |    ");
+            System.out.println("    O     |    ");
+            System.out.println("          |    ");
+            System.out.println("          |    ");
+            System.out.println("          |    ");
+            System.out.println("        -----  ");
+        }else if(liv == 4){
+            System.out.println("               ");
+            System.out.println("    -------    ");
+            System.out.println("    |     |    ");
+            System.out.println("    O     |    ");
+            System.out.println("    |     |    ");
+            System.out.println("          |    ");
+            System.out.println("          |    ");
+            System.out.println("        -----  ");
+        }else if(liv == 3){
+            System.out.println("               ");
+            System.out.println("    -------    ");
+            System.out.println("    |     |    ");
+            System.out.println("    O     |    ");
+            System.out.println("   /|     |    ");
+            System.out.println("          |    ");
+            System.out.println("          |    ");
+            System.out.println("        -----  ");
+        }else if(liv == 2){
+            System.out.println("               ");
+            System.out.println("    -------    ");
+            System.out.println("    |     |    ");
+            System.out.println("    O     |    ");
+            System.out.println("   /|\\    |    ");
+            System.out.println("          |    ");
+            System.out.println("          |    ");
+            System.out.println("        -----  ");
+        }else if(liv == 1){
+            System.out.println("               ");
+            System.out.println("    -------    ");
+            System.out.println("    |     |    ");
+            System.out.println("    O     |    ");
+            System.out.println("   /|\\    |    ");
+            System.out.println("   /      |    ");
+            System.out.println("          |    ");
+            System.out.println("        -----  ");
+        }else if(liv == 0){
+            System.out.println("               ");
+            System.out.println("    -------    ");
+            System.out.println("    |     |    ");
+            System.out.println("    O     |    ");
+            System.out.println("   /|\\    |    ");
+            System.out.println("   / \\    |    ");
+            System.out.println("          |    ");
+            System.out.println("        -----  ");
+        }
 
     }
 
